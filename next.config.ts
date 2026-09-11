1c6ff61b181f6ecd1697ebafb77325fa2d1b698e
Shawn Inder <shawninder@gmail.com>
2026-09-11 19:41:57 -0400

    mobile dev fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: 'export'
+  output: 'export',
+  allowedDevOrigins: ['192.168.*.*', '10.*.*.*']
 }
 
 export default nextConfig
