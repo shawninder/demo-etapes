101df1fab81b9b311fc967e1913ee18fbce24f99
Shawn Inder <shawninder@gmail.com>
2026-09-11 15:12:15 -0400

    start using shadcn ui (Item)

--- src/components/River.tsx
+++ src/components/River.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import type { RiverFeature } from "@/app/[slug]/features"
+import { Item, ItemGroup, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
 
 export type RiverProps = {
   features: RiverFeature[]
@@ -39,40 +40,64 @@ export default function River ({ features = [] }: RiverProps) {
     .sort((a, b) => b - a)
 
   let dayCounter = 1
+
   return (
-    <div className='w-full pl-5 feature-list'>
+    <ItemGroup className='w-full feature-list gap-0'>
+      <Day dayCounter={dayCounter++} />
       {features.map(({ id, km, label, text }) => {
         if (label === '') {
           return null
         }
         const kmsTravelled = checked[km] ? distanceFromLastChecked(parseFloat(km), sortedChecked) : null
-        const daySummary = checked[km] ? featuresEncountered(parseFloat(km), sortedChecked, features) : null
+        const daySummary = checked[km] ? countFeaturesEncountered(parseFloat(km), sortedChecked, features) : null
+        const isCampable = label.indexOf('🏕') !== -1 || label.indexOf('🚙') !== -1
         return (
-          <div key={id}>
-            {(label.indexOf('🏕') !== -1 || label.indexOf('🚙') !== -1) && (kmsTravelled || daySummary) ? (
-              <div className='text-right'>
-                <span>Jour {dayCounter++}: </span>
-                {kmsTravelled}{" – "}{daySummary}
-              </div>
-            ) : null}
-            <div title={text}>
-              <span className='text-xs text-muted w-16 inline-block font-mono'>km {parseFloat(km).toFixed(1)}</span>
-              {label.indexOf('🏕') !== -1 || label.indexOf('🚙') !== -1
+          <Fragment key={id}>
+            <Item variant='outline' title={text} className='py-0.5 rounded-none hover:bg-muted'>
+              <ItemMedia variant='icon' className='text-xs text-muted-foreground w-16 inline-block font-mono'>km {parseFloat(km).toFixed(1)}</ItemMedia>
+              <ItemContent className='flex flex-row items-center gap-2'>
+                <ItemTitle className='w-14'>
+                  {label[0] === 'R' || label[0] === 'C' || label[0] === 'L' || label[0] === 'R' || label[0] === 'E'
+                    ? '🌊'
+                    : null
+                  }{" "}{label}
+                </ItemTitle>
+                <ItemDescription className='italic text-muted-foreground'>{text}</ItemDescription>
+              </ItemContent>
+              {isCampable
                 ? (
-                  <input type='checkbox' checked={checked[km] || false} onChange={onChange} data-km={km} />
+                  <ItemActions>
+                    <input type='checkbox' checked={checked[km] || false} onChange={onChange} data-km={km} />
+                  </ItemActions>
                 )
                 : null
               }
-              {label[0] === 'R' || label[0] === 'C' || label[0] === 'L' || label[0] === 'R' || label[0] === 'E'
-                ? '🌊'
-                : null
-              } {label}
-              {" – "}<span className='italic text-muted'>{text}</span>
-            </div>
-          </div>
+            </Item>
+            {isCampable && (kmsTravelled || daySummary) ? (
+              <Item className=''>
+                <ItemContent>
+                  <ItemTitle>Sommaire: </ItemTitle>
+                  <ItemDescription>
+                    <span>{kmsTravelled}</span>
+                    <span>{" avec "}</span>
+                    <ItemGroup className='flex-row w-fit'>
+                      {daySummary && Object.entries(daySummary).map(([label, count]) => (
+                        <Item key={label} className='flex flex-row items-center gap-2'>
+                          <ItemTitle><span className=''>{count}{" "}⨉</span> <span className='text-lg border border-accent inline-block p-2 rounded'>{label}</span></ItemTitle>
+                        </Item>
+                      ))}
+                    </ItemGroup>
+                  </ItemDescription>
+                </ItemContent>
+              </Item>
+            ) : null}
+            {isCampable && (kmsTravelled || daySummary) ? (
+              <Day dayCounter={dayCounter++} />
+            ) : null}
+          </Fragment>
         )
       })}
-    </div>
+    </ItemGroup>
   )
 }
 
@@ -94,10 +119,10 @@ function distanceFromLastChecked(km: number, sortedChecked: number[]) {
   }
   const distance = lastKm - km
 
-  return lastKm === Infinity ? null : `~${Math.round(distance)} km`
+  return lastKm === Infinity ? null : `${Math.round(10 * distance)/10} km`
 }
 
-function featuresEncountered(km: number, sortedChecked: number[], features: RiverFeature[]) {
+function countFeaturesEncountered(km: number, sortedChecked: number[], features: RiverFeature[]) {
   if (sortedChecked.length === 0) {
     return null
   }
@@ -114,7 +139,7 @@ function featuresEncountered(km: number, sortedChecked: number[], features: Rive
     }
   }
 
-  const featuresEncountered = features.reduce<Record<string, number>>((acc, feature) => {
+  return features.reduce<Record<string, number>>((acc, feature) => {
     const featureKm = parseFloat(feature.km)
     if (featureKm > km
       && featureKm <= lastKm
@@ -128,8 +153,10 @@ function featuresEncountered(km: number, sortedChecked: number[], features: Rive
       }
     return acc
   }, {})
+}
 
-  return Object.entries(featuresEncountered)
-    .map(([label, count]) => `${count} ${label}`)
-    .join(', ')
+function Day ({ dayCounter }: { dayCounter: number }) {
+  return (
+    <Item className='font-bold justify-center'>Jour {dayCounter}</Item>
+  )
 }
